8f27b090540d57a6b6df1a670e050bbc7ff1c786
Anthonyg6 <anthonygallegos6@gmail.com>
2019-07-18 17:34:57 -0600

    built form button component in form fields js and added submit login button to login

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -3,10 +3,13 @@ import React, { Component } from "react";
 import SignInForm from "./signinForm";
 
 class SignIn extends Component {
+  onSubmit = fields => {
+    console.log(fields);
+  };
   render() {
     return (
       <div className="sign-in">
-        <SignInForm className="sign-in-form" />
+        <SignInForm onSubmit={this.onSubmit} className="sign-in-form" />
       </div>
     );
   }
--- src/components/formFields.js
+++ src/components/formFields.js
@@ -5,13 +5,31 @@ export class FormInput extends Component {
     const { className, title, type, placeholder, input } = this.props;
     return (
       <div className={`${className} form-input`}>
-        <label className="form-input-lable">{title}</label>
+        <label className="form-input-label">{title}</label>
         <input
           className="form-input-input"
           type={type}
           placeholder={placeholder}
           {...input}
         />
+      </div>
+    );
+  }
+}
+
+export class FormButton extends Component {
+  render() {
+    const { className, title, type, onClick, input } = this.props;
+    return (
+      <div className={`${className} form-button`}>
+        <button
+          className={`form-button-button`}
+          type="type"
+          {...input}
+          onClick={onclick}
+        >
+          {title}
+        </button>
       </div>
     );
   }
