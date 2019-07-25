9b694d6005a145fc49fe4afc2a6e2d0919b562ea
Anthonyg6 <anthonygallegos6@gmail.com>
2019-07-25 05:47:57 -0600

    built structure of account related components

---
+++ src/components/account/account.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Account extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Content</h1>
+      </div>
+    );
+  }
+}
