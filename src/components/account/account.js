52d11156ed5314691f2efae24c1b64515bb0c15a
Anthonyg6 <anthonygallegos6@gmail.com>
2019-07-25 19:40:18 -0600

    developed navbar and header actions for setting new links in account files

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,11 +1,55 @@
 import React, { Component } from "react";
 
-export default class Account extends Component {
+import { connect } from "react-redux";
+import * as actions from "../../actions/index";
+
+import PurchaseHistory from "./purchaseHistory";
+import AccountInformation from "./accountInformation";
+
+class Account extends Component {
+  componentDidMount() {
+    const navbarLinks = [
+      {
+        id: 0,
+        title: "Purchase History",
+        active: false,
+        component: <PurchaseHistory />
+      },
+      {
+        id: 1,
+        title: "Account Information",
+        active: true,
+        component: <AccountInformation />
+      }
+    ];
+
+    this.props.setHeaderLinks([]);
+    this.props.setNavbarLinks(navbarLinks);
+  }
+
+  renderContent() {
+    let jsx;
+    if (this.props.navbarLinks) {
+      this.props.navbarLinks.map(link => {
+        if (link.active) {
+          jsx = link.component;
+        }
+      });
+    }
+    return jsx;
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Content</h1>
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
+function mapStateToProps(state) {
+  const { headerLinks, navbarLinks } = state.headerNavbar;
+  return { headerLinks, navbarLinks };
+}
+Account = connect(
+  mapStateToProps,
+  actions
+)(Account);
+
+export default Account;
