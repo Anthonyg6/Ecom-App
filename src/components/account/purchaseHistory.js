139e255839111c0b4a9b206253ef0b508508226a
Anthonyg6 <anthonygallegos6@gmail.com>
2019-07-26 21:18:02 -0600

    began to build out the children of the account components and set up some basic styles

--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 import PageTitle from "../pageTitle";
+import Purchases from "./purchases";
+import PurchaseDetail from "./purchasesDetail";
 
 class PurchaseHistory extends Component {
   render() {
@@ -10,6 +12,9 @@ class PurchaseHistory extends Component {
           className="purchase-history-page-title"
           title="Purchase History"
         />
+        <Purchases className="purchase-history-purchases" />
+        <PurchaseDetail className="purchase-history-purchase-detail" />
+        <div className="purchase-history-bottom-border" />
       </div>
     );
   }
