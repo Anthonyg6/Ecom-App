9e054866c738f39c8569f914e1b2ec68e777ebb6
Anthonyg6 <anthonygallegos6@gmail.com>
2019-08-02 22:13:23 -0600

    built base for the shop component and created a route for the shop component

---
+++ src/components/shop/shop.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+import * as actions from "../../actions";
+
+class Shop extends Component {
+  componentDidMount() {
+    const headerLinks = [
+      {
+        id: 0,
+        title: "Login",
+        path: "/signin"
+      }
+    ];
+    this.props.setHeaderLinks(headerLinks);
+    // Fetch shop products action creater
+    // set the header links
+    // set the navbar links
+  }
+  render() {
+    return (
+      <div className="shop">
+        shop...... shop......
+        <p>shop...</p>
+        {/* shop search bar*/}
+        {/* shop product */}
+        {/* shop cart button*/}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return state;
+}
+Shop = connect(
+  mapStateToProps,
+  actions
+)(Shop);
+
+export default Shop;
