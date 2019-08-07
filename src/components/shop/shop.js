90b4000d0ffa542ca5b47ab7a568eba11e385ffe
Anthonyg6 <anthonygallegos6@gmail.com>
2019-08-06 20:36:39 -0600

    filtered feature with products complete

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -28,10 +28,19 @@ class Shop extends Component {
   render() {
     return (
       <div className="shop">
-        shop...... shop......
-        <p>shop...</p>
         {/* shop search bar*/}
-        {/* shop product */}
+        <div className="shop-products">
+          {this.props.filteredProducts.map(product => {
+            return (
+              <div key={product.id} className="shop-product">
+                <div className="shop-product-title">{product.title}</div>
+                <div className="shop-product-description">
+                  {product.description}
+                </div>
+              </div>
+            );
+          })}
+        </div>
         {/* shop cart button*/}
       </div>
     );
@@ -39,9 +48,10 @@ class Shop extends Component {
 }
 
 function mapStateToProps(state) {
-  const { categories } = state.shop;
+  const { categories, filteredProducts } = state.shop;
   return {
-    categories
+    categories,
+    filteredProducts
   };
 }
 Shop = connect(
