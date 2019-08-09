27d7d90f72afbe80157f3445b1b58846198a8bbe
Anthonyg6 <anthonygallegos6@gmail.com>
2019-08-08 21:12:03 -0600

    built begining of shop grid and finished off the back of the shop card

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -64,7 +64,7 @@ export function fetchShopProducts() {
         id: 0,
         title: "Javascript in the Browser",
         description:
-          "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+          "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur?",
         price: 1.99,
         belongsTo: [0, 1]
       },
