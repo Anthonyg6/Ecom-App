384b27613db3548c78559a6a42fc7a23a4eaa4c8
Anthonyg6 <anthonygallegos6@gmail.com>
2019-08-08 22:04:20 -0600

    created and added flip card feature of the shop products and image overlay

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -7,11 +7,20 @@ class ShopProducts extends Component {
     const { id, title, description, price } = this.props;
     return (
       <div className="shop-product">
-        <div className="shop-product-title">{title}</div>
-        <div className="shop-product-description">{description}</div>
-        <GreenPriceTag className="shop-product-price" title={price} />
-        <Quantity className="shop-product-quantity" quantity={1} />
-        <div className="shop-product-add-to-cart">Add to Cart</div>
+        <div className="shop-product-front">
+          <img
+            className="shop-product-front-img"
+            src="https://via.placeholder.com/200x200"
+          />
+          <div className="shop-product-front-title">{title}</div>
+        </div>
+        <div className="shop-product-back">
+          <div className="shop-product-back-title">{title}</div>
+          <div className="shop-product-back-description">{description}</div>
+          <GreenPriceTag className="shop-product-back-price" title={price} />
+          <Quantity className="shop-product-back-quantity" quantity={1} />
+          <div className="shop-product-back-add-to-cart">Add to Cart</div>
+        </div>
       </div>
     );
   }
